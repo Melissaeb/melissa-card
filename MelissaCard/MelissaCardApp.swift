52f6e5c37e3917e5314b17a8fe3206b1332d8833
Melissa Elliston-Boyes <melissa.elliston-boyes@trademe.co.nz>
2024-11-22 13:44:33 +1300

    Initial Commit

---
+++ MelissaCard/MelissaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MelissaCardApp.swift
+//  MelissaCard
+//
+//  Created by Melissa Elliston-Boyes on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MelissaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MelissaCardTests/MelissaCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  MelissaCardTests.swift
+//  MelissaCardTests
+//
+//  Created by Melissa Elliston-Boyes on 22/11/2024.
+//
+
+import Testing
+@testable import MelissaCard
+
+struct MelissaCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
